c6540c54a76ccca283d453b9e53be6a301c45c5e
Adonys <avaldesa007@gmail.com>
2021-01-02 16:32:52 -0400

    First Commit

---
+++ application/config/paypal.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+It's highly recommended to use sandbox account for testing purpose
+*/
+
+$config['mode']          = "sandbox"; //for live account use the key "live"
+$config['client_id']     = 'Aep6eo1at8VldciR7Vv3E5-vftGugk5jcLh6uZJQykVsHcQm_uw8SFOaifWauQr2B7-erBI-r-VufSQ1'; //provide your client ID
+$config['client_secret'] = 'EFCKngNH2xN31SCYVD5j-ee_I3aj_G0aw6y4pX87u3Y-gL8Ct5UxKIMN1OjaDiK_gi6AbwsWu1CQL6sX'; //provide your secret ID
+$config['currency']      = 'USD'; //provide the currency code you want to use
